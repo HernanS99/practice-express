397e641eee52905e0eff7d4472bcff3b4012f50d
HernanS99 <hernan.silva2299@gmail.com>
2022-10-17 16:52:22 -0300

    add export and import to routes

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const { response } = require('express')
 const express = require('express')
+const router = require('./routes/index')
 
 const app =  express()
 
-app.get('/componentes',(req,res) => {
-    res.json({name:'rtx 3090',precio: 34000, stock: 32})
-})
-app.post('/nuevo',(req,res)=>{
-    res.json({success: true, response: 'Componente insertado correctamente'})
-})
+app.use('/api',router)
 
-app.get('/componentes/:tarjetasgraficas',(req,res)=>{
-    res.json({success: true, response:'listado de tarjetas de video'})
-})
 
 
 app.listen(4000,()=>console.log('Todo bien'))
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-router.Router('/productos')
+router.route('/productos')
     .post(()=> console.log('post'))
     .get(()=>console.log('get'))
 
